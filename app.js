823dc2ffcc08c174b80a16a9771899c8f6edfa9a
ericstotle93 <elu1993@gmail.com>
2018-01-22 11:16:35 -0500

    Set up Express to serve static React files. Created postinstall script for Heroku to build static React app.

--- app.js
+++ app.js
@@ -7,11 +7,7 @@ const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
-const index = require('./routes/index')
-const users = require('./routes/users')
-
-const app = express();
+const app = express()
 
 //db setup
 mongoose.Promise = global.Promise
@@ -22,9 +18,16 @@ connection.on('connected', () => {
   console.log('Mongoose Connected Successfully')
 })
 
+
 // If the connection throws an error
 connection.on('error', (err) => {
   console.log('Mongoose default connection error: ' + err)
+})
+
+// serve static react files
+app.use(express.static(__dirname + '/client/build/'));
+app.get('/', (req,res) => {
+  res.sendFile(__dirname + '/client/build/index.html')
 })
 
 // view engine setup
@@ -39,9 +42,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', index)
-app.use('/users', users)
-
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found')
@@ -53,7 +53,7 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
 
   // render the error page
   res.status(err.status || 500)
