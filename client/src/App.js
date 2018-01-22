dc3ad73eda82a15ecd288fc1e8da620db5ed7fc8
ericstotle93 <elu1993@gmail.com>
2018-01-22 15:11:57 -0500

    Created initial react components for view of app

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
+import styled from 'styled-components'
+import HomePage from './components/HomePage'
+import UserPage from './components/UserPage'
+import InterestPage from './components/InterestPage'
+import EventPage from './components/EventPage'
 
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path="/" component={HomePage}/>
+            {/* <Route path="/login" component={LogInPage}/>
+            <Route path="/user/:userId" component={InterestPage}/> */}
+          </Switch>
+        </div>
+      </Router>
+    )
   }
 }
 
